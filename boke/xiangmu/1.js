6ea791fa7029a14009505f5a3848785016ca0a7f
zhaoname666 <131731963+zhaoname666@users.noreply.github.com>
2023-06-18 15:30:59 +0800

    Add files via upload

---
+++ boke/xiangmu/1.js
@@ -0,0 +1,122 @@
+arr = [{
+    id: 1,
+    title: "开发随机",
+    user: "📒记录每日开发中的一些碎片，持续更新",
+    time: 3,
+    label: "日常开发"
+},
+{
+    id: 2,
+    title: "RocketMQ分布式事务原理",
+    user: "📒分布式事务常见的方案有TCC(Try-Confirm-Cancel)，XA两阶段提交方案，可靠消息最终一致性方案，最大努力通知方案等等。其中可靠消息最终一致性方案主要就可以依靠RoketMQ来做，因为RocketMQ支持消息事务。先上一张图: Roc..",
+    time: 5,
+    label: "java",
+},
+{
+    id: 3,
+    title: "RocketMQ消息丢失场景及解决办法",
+    user: "📒记录每日开发中的一些碎片，持续更新",
+    time: 3,
+    label: "工具"
+},
+{
+    id: 4,
+    title: "Mysql: 用覆盖索引解决回表查询",
+    user: "📒记录每日开发中的一些碎片，持续更新",
+    time: 3,
+    label: "label"
+},
+{
+    id: 5,
+    title: "Java Spring Bean的生命周期",
+    user: "📒记录每日开发中的一些碎片，持续更新",
+    time: 3,
+    label:  "工具"
+},
+{
+    id: 6,
+    title: "自己感觉好用的工具列表",
+    user: "📒记录每日开发中的一些碎片，持续更新",
+    time: 3,
+    label: "日常开发",
+  }, {
+    id: 7,
+    title: "ChatGPT太太太太太香了",
+    user: "📒记录每日开发中的一些碎片，持续更新",
+    time: 3,
+    label: "java",
+  },
+{
+    id: 8,
+    title: "这年头，谁的好友列表还没有躺一个ChatGPT啊？",
+    user: "📒记录每日开发中的一些碎片，持续更新",
+    time: 3,
+    label: "label"
+},
+{
+    id: 9,
+    title: "确定了，今天带一个看板娘回家",
+    user: "📒记录每日开发中的一些碎片，持续更新",
+    time: 3,
+    label:  "java"
+},]
+     arr1 = [
+      {
+    id: 1,
+    title: "开发随机",
+    user: "📒记录每日开发中的一些碎片，持续更新",
+    time: 3,
+    label: "日常开发"
+},
+{
+    id: 2,
+    title: "RocketMQ分布式事务原理",
+    user: "📒分布式事务常见的方案有TCC(Try-Confirm-Cancel)，XA两阶段提交方案，可靠消息最终一致性方案，最大努力通知方案等等。其中可靠消息最终一致性方案主要就可以依靠RoketMQ来做，因为RocketMQ支持消息事务。先上一张图: Roc..",
+    time: 5,
+    label: "java",
+},
+{
+    id: 3,
+    title: "RocketMQ消息丢失场景及解决办法",
+    user: "📒既然在项目中使用了MQ，那么就不可避免的需要考虑消息丢失问题",
+    time: 3,
+    label: "工具"
+},
+{
+    id: 4,
+    title: "Mysql: 用覆盖索引解决回表查询",
+    user: "📒既然在项目中使用了MQ，那么就不可避免的需要考虑消息丢失问题。",
+    time: 3,
+    label: "label"
+},
+];
+asfdsf= [
+      {
+    id: 1,
+    title: "开发随机",
+    user: "📒记录每日开发中的一些碎片，持续更新",
+    time: 3,
+    label: "日常开发"
+},
+{
+    id: 2,
+    title: "RocketMQ分布式事务原理",
+    user: "📒分布式事务常见的方案有TCC(Try-Confirm-Cancel)，XA两阶段提交方案，可靠消息最终一致性方案，最大努力通知方案等等。其中可靠消息最终一致性方案主要就可以依靠RoketMQ来做，因为RocketMQ支持消息事务。先上一张图: Roc..",
+    time: 5,
+    label: "java",
+},
+{
+    id: 3,
+    title: "RocketMQ消息丢失场景及解决办法",
+    user: "📒既然在项目中使用了MQ，那么就不可避免的需要考虑消息丢失问题",
+    time: 3,
+    label: "工具"
+},
+{
+    id: 4,
+    title: "Mysql: 用覆盖索引解决回表查询",
+    user: "📒既然在项目中使用了MQ，那么就不可避免的需要考虑消息丢失问题。",
+    time: 3,
+    label: "label"
+},
+    ];
